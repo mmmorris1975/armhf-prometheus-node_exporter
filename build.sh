73e3523591c7456a71dd905b1f0418245bec994f
Mike Morris <michael.m.morris@gmail.com>
2017-02-27 10:40:29 -0600

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+ver=${VERSION:="0.14.0-rc.1"}
+
+NAME=$(basename $(dirname $PWD/Dockerfile))
+URL="https://github.com/prometheus/node_exporter/releases/download/v${ver}/node_exporter-${ver}.linux-armv7.tar.gz"
+
+wget -N $URL
+tar xzf node_exporter-${ver}.linux-armv7.tar.gz --strip-components 1 --wildcards "*/node_exporter"
+
+# It's better to run this directly on the node vs. inside a container, since we'd need to do all sorts of gymnastics
+# to get the host OS stats exposed to the container.  Run install.sh instead to install as a system service
+#docker build -t ${NAME}:${ver} -t ${NAME}:latest .
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+function cp_set_perms() {
+  src=$1
+  dest=$2
+  mode=$3
+  owner=$4
+
+  sudo cp $src $dest
+  sudo chown $owner $dest
+  sudo chmod $mode $dest
+}
+
+REPO_DIR=$(dirname $0)
+INSTALL_DIR=/usr/local/prometheus/bin
+
+sudo mkdir -m 0755 -p $INSTALL_DIR
+sudo chown root:root $INSTALL_DIR
+
+cp_set_perms $REPO_DIR/node_exporter $INSTALL_DIR/ 0755 root:root
+
+if [ $(ps -q 1 -o comm=) == systemd ]
+then
+  cp_set_perms $REPO_DIR/systemd-node_exporter.service /etc/systemd/system/node_exporter.service 0644 root:root
+  sudo systemctl enable node_exporter.service
+else
+  echo "TODO: add install steps for other init systems (SysV/upstart would be next)"
+fi
